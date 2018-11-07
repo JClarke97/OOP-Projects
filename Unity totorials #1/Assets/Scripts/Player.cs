4e54ad28d390ea56b27c36f437b922643be5808f
JClarke97 <jesseclarke97@hotmail.com>
2018-11-07 10:49:36 +0000

    add tutorial project 1

---
+++ Unity totorials #1/Assets/Scripts/Player.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    [SerializeField]
+    private stat Experiance;
+
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            Experiance.CurrentVal -= 10;
+        }
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            Experiance.CurrentVal += 10;
+            
+}
---
+++ Unity totorials #1/Assets/Scripts/UIscript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIscript : MonoBehaviour {
+
+    [SerializeField]
+    private Text valueText;
+    public float Value
+    {
+        set
+        {
+            valueText.text = Value,0,MaxValue;
+            ;
+
+        }
+          // Use this for initialization
+    void Start()
+    {
+       
+
+
+
+
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
